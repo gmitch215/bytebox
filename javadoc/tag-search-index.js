5788cecaa3d926ffcb173081efbca95ebda2804e
GitHub Action <action@github.com>
2026-08-28 21:04:10 +0000

    Update Javadoc (a2e43f9)

--- javadoc/tag-search-index.js
+++ javadoc/tag-search-index.js
@@ -1,1 +1,1 @@
-tagSearchIndex = [{"l":"Constant Field Values","h":"","u":"constant-values.html"}];updateSearchResults();
+tagSearchIndex = [{"l":"Constant Field Values","h":"","u":"constant-values.html"},{"l":"Numbers","h":"interface dev.gmitch215.bytebox.js.TSObject","d":"Section","u":"dev/gmitch215/bytebox/js/TSObject.html#numbers-heading"},{"l":"Serialized Form","h":"","u":"serialized-form.html"}];updateSearchResults();
